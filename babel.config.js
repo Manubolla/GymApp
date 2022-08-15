516a99435d16cf685b3389efa287c909841e5622
Manubolla <manubolla17@gmail.com>
2022-08-15 14:00:27 -0300

    add firebase and dot env

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function (api) {
   api.cache(true);
+  const envName = process.env.APP_ENV || "dev";
+  const dotEnvPath = path.resolve(__dirname, `.env.${envName}`);
+  if (
+    !fs.existsSync(dotEnvPath) &&
+    !process.env.EAS_BUILD &&
+    !process.env.GITHUB_ACTIONS
+  ) {
+    throw new Error(
+      `Babel config couldn't find dot env file path: ${dotEnvPath}`
+    );
+  }
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "inline-dotenv",
+      {
+        unsafe: true,
+      },
+    ],
   };
 };
